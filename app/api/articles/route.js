377b5227b0cc4849a87b3e9835867e30402faaf4
stefanpython <stefanhtml@yahoo.com>
2024-04-04 16:09:36 +0300

    Implement DELETE api route

--- app/api/articles/route.js
+++ app/api/articles/route.js
@@ -10,8 +10,15 @@ export async function POST(req) {
   return NextResponse.json({ message: "Article created" }, { status: 201 });
 }
 
-export async function GET(req) {
+export async function GET() {
   await connectMongoDB();
   const articles = await Article.find();
   return NextResponse.json({ articles });
+}
+
+export async function DELETE(req) {
+  const id = req.nextUrl.searchParams.get("id");
+  await connectMongoDB();
+  await Article.findByIdAndDelete(id);
+  return NextResponse.json({ message: "Article deleted" }, { status: 200 });
 }
