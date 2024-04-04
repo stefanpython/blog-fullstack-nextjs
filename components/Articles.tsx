c3caecf337095187df1d56e3b60309b2e006840c
stefanpython <stefanhtml@yahoo.com>
2024-04-04 16:22:11 +0300

    Add and link edit button on articles

--- components/Articles.tsx
+++ components/Articles.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import Image from "next/image";
 import { FaRegUser } from "react-icons/fa";
+import Link from "next/link";
 
 export default function Articles() {
   return (
@@ -10,7 +11,13 @@ export default function Articles() {
       <div className="flex flex-wrap gap-10">
         <div className="flex  py-10 max-w-[30em]">
           <div className="flex flex-col px-1">
-            <Image src="/frog.jpg" width={500} height={500} alt="frog" />
+            <Image
+              src="/frog.jpg"
+              width={500}
+              height={500}
+              alt="frog"
+              priority={true}
+            />
 
             <h1 className="text-2xl py-2 hover:underline">Title goes here</h1>
 
@@ -22,10 +29,18 @@ export default function Articles() {
               pariatur temporibus.
             </p>
 
-            <div className="flex items-center">
-              <FaRegUser size={20} />
-              <span className="flex p-1">by: </span>
-              <p>Nelson Mandela</p>
+            <div className="flex justify-between">
+              <div className="flex items-center">
+                <FaRegUser size={20} />
+                <span className="flex p-1">by: </span>
+                <p>Nelson Mandela</p>
+              </div>
+
+              <Link href="/editArticle/id">
+                <button className="btn border px-4 py-1 bg-green-400 text-white font-bold rounded-sm">
+                  Edit
+                </button>
+              </Link>
             </div>
           </div>
         </div>
