63bf178da6512f2c65fbfdce80c5b6865cdab419
stefanpython <stefanhtml@yahoo.com>
2024-04-04 14:31:15 +0300

    Create and link addArticle route

---
+++ app/addArticle/page.tsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function AddArticle() {
+  return <div>AddArticle</div>;
+}
--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -2,16 +2,18 @@ import Link from "next/link";
 
 export default function Navbar() {
   return (
-    <nav className="flex justify-between items-center px-8 py-10">
+    <nav className="flex justify-between items-center px-8 py-10 border shadow-lg">
       <Link className="text-black font-bold text-3xl" href={"/"}>
         Blog NextJS.
       </Link>
 
       <p className="text-lg">A full stack nextjs blog website</p>
 
-      <button className="btn border p-2 rounded-md bg-gray-300 font-medium hover:bg-gray-400">
-        Add post
-      </button>
+      <Link href="/addArticle">
+        <button className="btn border p-2 rounded-md bg-gray-300 font-medium hover:bg-gray-400">
+          Add post
+        </button>
+      </Link>
     </nav>
   );
 }
