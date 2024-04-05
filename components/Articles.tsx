7d4532d61d6f4acb273541deaef5e7b0dc8b373b
stefanpython <stefanhtml@yahoo.com>
2024-04-05 12:59:27 +0300

    Link article

--- components/Articles.tsx
+++ components/Articles.tsx
@@ -11,16 +11,17 @@ export default function Articles() {
       <div className="flex flex-wrap gap-10">
         <div className="flex  py-10 max-w-[30em]">
           <div className="flex flex-col px-1">
-            <Image
-              src="/frog.jpg"
-              width={500}
-              height={500}
-              alt="frog"
-              priority={true}
-            />
-
-            <h1 className="text-2xl py-2 hover:underline">Title goes here</h1>
-
+            <Link href="/article/id">
+              {/* CHANGE DYNAMIC IP HERE */}
+              <Image
+                src="/frog.jpg"
+                width={500}
+                height={500}
+                alt="frog"
+                priority={true}
+              />
+              <h1 className="text-2xl py-2 hover:underline">Title goes here</h1>
+            </Link>
             <p>June 2, 2022</p>
 
             <p className="py-5">
@@ -36,6 +37,7 @@ export default function Articles() {
                 <p>Nelson Mandela</p>
               </div>
 
+              {/* CHANGE DYNAMIC IP HERE */}
               <Link href="/editArticle/id">
                 <button className="btn border px-4 py-1 bg-green-400 text-white font-bold rounded-sm">
                   Edit
