41768eb8f46fddad10566022758f1e9543709c37
stefanpython <stefanhtml@yahoo.com>
2024-04-17 17:42:58 +0300

    Check if user exists before registering

--- app/api/register/route.js
+++ app/api/register/route.js
@@ -6,17 +6,29 @@ import bcrypt from "bcryptjs";
 export async function POST(req) {
   try {
     const { email, password } = await req.json();
+
     const hashedPassword = await bcrypt.hash(password, 10);
 
     await connectMongoDB();
+
+    // Check for existing user with email and ID
+    const existingUser = await User.findOne({ email }).select("_id");
+
+    if (existingUser) {
+      return NextResponse.json(
+        { message: "User with this email and ID already exists." },
+        { status: 400 }
+      );
+    }
+
+    // Create user if not found
     await User.create({ email, password: hashedPassword });
 
     return NextResponse.json({ message: "User registered" }, { status: 200 });
   } catch (error) {
     return NextResponse.json(
       { message: "An error occured while registering the user." },
-      { status: 500 },
-      { error: error }
+      { status: 500 }
     );
   }
 }
